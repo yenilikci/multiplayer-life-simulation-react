acfce27ae07d1546a7d88fdf0a39240b990eb11b
yenilikci <m.melihcelik017@outlook.com>
2024-07-13 11:35:03 +0300

    feat(skeleton-utils): implemented SkeletonUtils clone for multiple skinned mesh models

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 
 import { AnimatedWoman } from "./AnimatedWoman";
 export const Experience = () => {
@@ -6,8 +6,14 @@ export const Experience = () => {
         <>
             <Environment preset="sunset" />
             <ambientLight intensity={0.3} />
+            <ContactShadows blur={2} />
             <OrbitControls />
             <AnimatedWoman />
+            <AnimatedWoman
+                position-x={1}
+                hairColor="red"
+                topColor="blue"
+            />
         </>
     );
 };
