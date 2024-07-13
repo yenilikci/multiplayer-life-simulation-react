88cda1a2d98bbb59a7ff71ed4b48fc574c259fea
yenilikci <m.melihcelik017@outlook.com>
2024-07-13 10:56:41 +0300

    feat(glb-to-jsx): converted animated model to jsx component

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,13 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
 
+import { AnimatedWoman } from "./AnimatedWoman";
 export const Experience = () => {
     return (
         <>
             <OrbitControls />
-            <mesh>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <AnimatedWoman />
         </>
     );
 };
