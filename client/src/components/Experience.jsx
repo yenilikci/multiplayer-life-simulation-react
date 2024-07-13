f7a1c0c1549deb926836b1481a26c5f2fd8397f3
yenilikci <m.melihcelik017@outlook.com>
2024-07-13 11:25:10 +0300

    feat(environment-sunset): added sunset environment and ambientLight

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,9 +1,11 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 import { AnimatedWoman } from "./AnimatedWoman";
 export const Experience = () => {
     return (
         <>
+            <Environment preset="sunset" />
+            <ambientLight intensity={0.3} />
             <OrbitControls />
             <AnimatedWoman />
         </>
