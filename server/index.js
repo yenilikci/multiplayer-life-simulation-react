edbfafb750ce7807cd67e29554b9dba033ccde4c
yenilikci <m.melihcelik017@outlook.com>
2024-07-14 19:25:37 +0300

    feat(pathfinding): added pathfinding and grid update

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import pathfinding from "pathfinding";
 
 const io = new Server({
     cors: {
@@ -409,6 +410,42 @@ const map = {
         },
     ],
 };
+
+const grid = new pathfinding.Grid(map.size[0] * map.gridDivision, map.size[1] * map.gridDivision);
+
+const finder = new pathfinding.AStarFinder({
+    allowDiagonal: true,
+    dontCrossCorners: true,
+});
+
+const findPath = (start, end) => {
+    const gridClone = grid.clone();
+    const path = finder.findPath(start[0], start[1], end[0], end[1], gridClone);
+    return path;
+};
+
+const updateGrid = () => {
+    map.items.forEach((item) => {
+        if (item.walkable || item.wall) {
+            return;
+        }
+        const width =
+            item.rotation === 1 || item.rotation === 3 ? item.size[1] : item.size[0];
+        const height =
+            item.rotation === 1 || item.rotation === 3 ? item.size[0] : item.size[1];
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                grid.setWalkableAt(
+                    item.gridPosition[0] + x,
+                    item.gridPosition[1] + y,
+                    false
+                );
+            }
+        }
+    });
+};
+
+updateGrid();
 
 const generateRandomPosition = () => {
     return [Math.random() * map.size[0], 0, Math.random() * map.size[1]];
